8b74edd5fbb08d0d85c3ae8b8bf238b7946b93e1
rachell-98 <79581369+rachell-98@users.noreply.github.com>
2021-03-17 16:16:59 +0800

    Add files via upload

---
+++ business schema.sql
@@ -0,0 +1,15 @@
+use business;
+
+create table business
+
+(
+BID INT primary key auto_increment,
+BName VARCHAR (128),
+Email VARCHAR(128),
+password VARCHAR(128),
+Paypal VARCHAR (128),
+BAddress VARCHAR (128),
+BDescription VARCHAR (128)
+);
+
+
