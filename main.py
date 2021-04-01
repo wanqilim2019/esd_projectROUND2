8fdac44bd49c2d3d2e3c3c0d631346fcd12dc07a
wanqilim <wanqilim99@hotmail.com>
2021-04-01 23:33:14 +0800

    linked signup and landing page

--- main.py
+++ main.py
@@ -1,111 +1,21 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask import Flask, render_template,redirect, url_for, request
+import requests
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/product'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-CORS(app)  
+app.config["DEBUG"] = True
+# auto reload
 
 
-
-@app.route("/product")
-def homepage():
-    productlist = Product.query.all()
-    if len(productlist):
-        return jsonify(
-            {
-                "code": 200,
-                "data": {
-                    "products": [product.json() for product in productlist]
-                }
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "message": "There are no products."
-        }
-    ), 404
+@app.route("/")
+def show_landing_page():
+    return render_template("index1.html")
 
 
-@app.route("/product/<string:pid>")
-def find_by_pid(pid):
-    product = Product.query.filter_by(pid=pid).first()
-    if product:
-        return jsonify(
-            {
-                "code": 200,
-                "data": product.json()
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "message": "Product not found."
-        }
-    ), 404
+@app.route("/signup")
+def show_signuppage():
+    return render_template('signup.html')
 
 
-
-
-
-# @app.route("/book/<string:isbn13>", methods=['PUT'])
-# def update_book(isbn13):
-#     book = Book.query.filter_by(isbn13=isbn13).first()
-#     if book:
-#         data = request.get_json()
-#         if data['title']:
-#             book.title = data['title']
-#         if data['price']:
-#             book.price = data['price']
-#         if data['availability']:
-#             book.availability = data['availability'] 
-#         db.session.commit()
-#         return jsonify(
-#             {
-#                 "code": 200,
-#                 "data": book.json()
-#             }
-#         )
-#     return jsonify(
-#         {
-#             "code": 404,
-#             "data": {
-#                 "isbn13": isbn13
-#             },
-#             "message": "Book not found."
-#         }
-#     ), 404
-
-
-# @app.route("/book/<string:isbn13>", methods=['DELETE'])
-# def delete_book(isbn13):
-#     book = Book.query.filter_by(isbn13=isbn13).first()
-#     if book:
-#         db.session.delete(book)
-#         db.session.commit()
-#         return jsonify(
-#             {
-#                 "code": 200,
-#                 "data": {
-#                     "isbn13": isbn13
-#                 }
-#             }
-#         )
-#     return jsonify(
-#         {
-#             "code": 404,
-#             "data": {
-#                 "isbn13": isbn13
-#             },
-#             "message": "Book not found."
-#         }
-#     ), 404
-
-
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
