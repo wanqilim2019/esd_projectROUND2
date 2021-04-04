c76f2f35c9321a284da2f76fa3f882efbdc36d8a
rachell-98 <79581369+rachell-98@users.noreply.github.com>
2021-04-04 11:33:38 +0800

    Update product.sql

--- database/product.sql
+++ database/product.sql
@@ -39,25 +39,25 @@ insert into product (PName,Price,PDescription,imgname)
 values('Handmade Adult Patchwork Clown Suit ', 63.18, "A vintage old circus clown suit for you next Halloween! ",img2.jpg);
 
 insert into product (PName,Price,PDescription,imgname)
-values(' Circus Party Box', 35.10, "Perfect for your circus themed party ! This product comes in pack of 12. ",img3.jpg);
+values('Circus Party Box', 35.10, "Perfect for your circus themed party ! This product comes in pack of 12. ",img3.jpg);
 
 insert into product (PName,Price,PDescription,imgname)
-values(' Mint soy wax Candle', 12.64, " Intended to draw prosperity, well-being, and fortune. Created with Eucalyptus Mint Natural Scent",img4.jpg);
+values('Mint soy wax Candle', 12.64, " Intended to draw prosperity, well-being, and fortune. Created with Eucalyptus Mint Natural Scent",img4.jpg);
 
 insert into product (PName,Price,PDescription,imgname)
-values(' Lavender Candle ', 23.86, " Secret Garden top two scents are Lavender and just a hit of Rose",img5.jpg);
+values('Lavender Candle ', 23.86, " Secret Garden top two scents are Lavender and just a hit of Rose",img5.jpg);
 
 insert into product (PName,Price,PDescription,imgname)
-values(' Lavender Goat Milk Soap All Natural Soap', 7.51, " Let scent of lavender whisk you away to the french countryside",img6.jpg);
+values('Lavender Goat Milk Soap All Natural Soap', 7.51, " Let scent of lavender whisk you away to the french countryside",img6.jpg);
 
 insert into product (PName,Price,PDescription,imgname)
-values(' Handmade Cold Process Soap', 5.62, " Love is in the air and what does it smell like? It smells beautiful, sweet & tropical.",img7.jpg);
+values('Handmade Cold Process Soap', 5.62, " Love is in the air and what does it smell like? It smells beautiful, sweet & tropical.",img7.jpg);
 
 insert into product (PName,Price,PDescription,imgname)
 values('Personalized Name Wood Puzzle Handmade Toy', 44.86, "We use special paint and varnish that is 100% safe for the babies! All of the puzzle pieces are raised, making them easy to get out and play with ",img8.jpg);
 
 insert into product (PName,Price,PDescription,imgname)
-values(' Handmade Dinosaur Crayons', 6.80, " These Dinosaur Crayons are perfect for your mini Dinosaur enthusiasts!",img9.jpg);
+values('Handmade Dinosaur Crayons', 6.80, " These Dinosaur Crayons are perfect for your mini Dinosaur enthusiasts!",img9.jpg);
 
 insert into product (PName,Price,PDescription,imgname)
 values('Knitted Handmade Bag', 97.86, "Your unique hand knitted bag that you will never find another same one!",img10.jpg);
