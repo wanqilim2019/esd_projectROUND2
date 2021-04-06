41e40c725ded679f39663e61749c0cd5b86b6443
wanqilim <wanqilim99@hotmail.com>
2021-04-06 08:05:26 +0800

    test product listing

--- database/business schema.sql
+++ database/business schema.sql
@@ -15,7 +15,7 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS  `ESD` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-USE `ESD`;
+USE `business`;
 
 DROP TABLE IF EXISTS business;
 CREATE TABLE business (
