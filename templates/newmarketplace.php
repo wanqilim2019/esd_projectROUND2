463bec872a6dc67fc76f9716444e2c000c026328
codedieme <cheahyewwei@gmail.com>
2021-04-14 01:32:57 +0800

    Update newmarketplace.php

--- templates/newmarketplace.php
+++ templates/newmarketplace.php
@@ -2,12 +2,8 @@
 <html lang="en">
 
 <head>
-
     <?php include "head.html" ?>
     <title>Marketplace</title>
-    <script src="https://www.paypal.com/sdk/js?client-id=AVL3t_B9WIEb-CdLDHYK1sW8nXEtp7GM8Qk9m29QTvb-OYyb2dxdo_PapsHFT5KI08OyWiWDNtuL9tI0&currency=SGD">
-        // Required. Replace YOUR_CLIENT_ID with your sandbox client ID.
-    </script>
 
     <style>
         .card:hover {
@@ -17,10 +13,18 @@
         }
     </style>
 
+    <!-- Paypal API -->
+    <script src="https://www.paypal.com/sdk/js?client-id=AVL3t_B9WIEb-CdLDHYK1sW8nXEtp7GM8Qk9m29QTvb-OYyb2dxdo_PapsHFT5KI08OyWiWDNtuL9tI0&currency=SGD">
+        // Required. Replace YOUR_CLIENT_ID with your sandbox client ID.
+    </script>
+
 </head>
 
 <body>
     <?php include "nav.html" ?>
+    <script>
+        console.log(window.sessionStorage);
+    </script>
 
     <section id="Header" class="pb-2">
         <div id="banner" class="container">
@@ -30,6 +34,17 @@
 
     <section id="market">
         <div class='container'>
+            <?php if (isset($_GET['msg'])) { ?>
+                <?php if ($_GET['msg'] == 'loggedin') { ?>
+                    <div class="alert alert-success" role="alert">
+                        Welcome back
+                    </div>
+                <?php } else if ($_GET['msg'] == 'congratulations') { ?>
+                    <div class="alert alert-success" role="alert">
+                        Congratulations ! You are registered!
+                    </div>
+                <?php } ?>
+            <?php } ?>
             <div class="row">
                 <div class="col-12 text-center">
                     <h1>Marketplace</h1>
@@ -115,7 +130,7 @@
                         cart.pop(i)
                     }
                 } else {
-                    console.log('else');
+                    //console.log('else');
                     let label = item.previousElementSibling;
                     let parent = item.parentElement;
                     parent.className = "btn btn-success";
@@ -177,20 +192,27 @@
 
         };
 
-
-
         var PAYPAL_CLIENT = 'AVL3t_B9WIEb-CdLDHYK1sW8nXEtp7GM8Qk9m29QTvb-OYyb2dxdo_PapsHFT5KI08OyWiWDNtuL9tI0';
         var PAYPAL_SECRET = 'EF1n5ItUXHKwB8qI7HRGzLtN9M5U9omdMKYTRm0BSvUkRgCr5LLyQvThBN4OJMl3a09yGFU8oXVf8Jpb';
         var PAYPAL_ORDER_API = 'https://api-m.paypal.com/v2/checkout/orders/';
 
         paypal.Buttons({
             createOrder: function(data, actions) {
-                var new_q = getQuantity()
+                var myCheckOutArr = myCart
+
+                var thePrice = 0
+
+                for (item of myCheckOutArr) {
+                    thePrice += parseFloat(item[2])
+                }
+
+                thePrice = parseFloat(thePrice).toFixed(2)
+
                 return actions.order.create({
                     purchase_units: [{
-                        description: thePName,
+                        description: "New Order",
                         amount: {
-                            value: thePrice * parseInt(new_q) // change to order price
+                            value: thePrice // change to order price
                         }
                     }] //end of purchase units
                 }); //end of return
@@ -203,7 +225,8 @@
                     console.log(details)
 
                     // Pass order info over to order microservice after transaction approves
-                    
+                    var placeOrderUrl = "http://127.0.0.1:5200/place_order"
+                    addNewOrder(placeOrderUrl);
 
                 }); //end of return
 
@@ -220,7 +243,7 @@
 
             // Add each PID of the order upon checkout to send over to order MS
             for (item of myCheckOutArr) {
-                pidArr.push(parseInt(myCheckOutArr[0]))
+                pidArr.push(parseInt(myCheckOutArr[0])) // [1,3,4]
             }
 
             var pidObj = {}
@@ -235,7 +258,7 @@
                 mode: 'cors', // no-cors, *cors, same-origin
                 cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
                 credentials: 'same-origin', // include, *same-origin, omit
-                origin: ["http://localhost:8080", "http://localhost:5001"],
+                origin: ["http://localhost:8080", "http://localhost:5200"],
                 redirect: 'follow' // manual, *follow, error
             });
             console.log('call');
@@ -244,8 +267,8 @@
                 console.log('response ok');
                 data = await response.json();
                 console.log(data);
-                productinfo = data.data;
-                window.location.href = ;
+                // productinfo = data.data;
+                // window.location.href = ;
 
             } else {
                 alert("There has been an error in listing the product, please refresh and try again");
@@ -255,6 +278,7 @@
     </script>
 
     <?php include "footer.html" ?>
+
 </body>
 
 
